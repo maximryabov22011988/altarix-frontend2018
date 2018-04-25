fc32e32cfa3b2d3be77924bfabadd0ee11ec07a7
Maxim Ryabov <designmr2015@gmail.com>
2018-04-25 18:56:01 +0400

    Добавляет базовую сборку на webpack

---
+++ webpack-template/src/js/index.js
@@ -0,0 +1,15 @@
+// import $ from 'jquery';
+import some from './some.js';
+
+// $('.title').html('Some text');
+
+console.log(some.avg(1, 4, 2));
+console.log(some.max(1, 4, 2));
+
+console.log(some.merge({
+  a: 1
+}, {
+  b: 20
+}));
+
+import '../css/style.css';
